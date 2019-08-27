9a57b2b75f2547c3b0abeafa21c779c4204d1a52
LHaisong <2896913077@qq.com>
2019-08-27 22:46:53 +0800

    牛客网删数

---
+++ src/com/prac/array/FindLastNumIndex.java
@@ -0,0 +1,41 @@
+package com.prac.array;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 牛客网删数
+ */
+public class FindLastNumIndex {
+    public static void main(String [] args) {
+        int len=9;
+        getIndex(len);
+    }
+/*-0 -1  -3 -4  6 -7*/
+    private static void getIndex(int len) {
+        List<Integer>list=new ArrayList<>();
+        for(int i=0;i<len;i++){
+            list.add(i);
+        }
+        int begin=-1;
+        for(int i=0;i<len; i++){
+            begin=begin+3;
+            if(list.size()==1) {
+                System.out.println(list.get(0));
+            } else {
+                if (begin >= list.size()) {
+                    begin -= list.size();
+                    while (begin>=list.size()){
+                        begin -= list.size();
+                    }
+                    list.remove(begin);
+                    begin--;
+                    System.out.println(list);
+                } else {
+                    list.remove(begin);
+                    System.out.println(list+"here");
+                    begin--;
+                }
+            }
+        }
+    }
+}
